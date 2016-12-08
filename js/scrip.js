93d798e881ba6403d4d7615c6ba402dfa5ce93b4
utsav3 <utsav.dhungel@gmail.com>
2016-12-08 02:24:07 -0600

    Inital commit with navbar and simple parallax

---
+++ js/scrip.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function() {
+    $(window).stellar();
+    
+});
+
+$(document).ready(
+
+  function() { 
+
+    $("html").niceScroll({
+        cursorcolor:"rgba(30,30,30,.5)",
+        zindex:999,
+        scrollspeed:100,
+        mousescrollstep:50,
+        cursorborder:"0px solid #fff",
+    });
+	
+	
+	
+			var ypos,image;
+			function parallex() {
+			image = document.getElementById('image');
+			ypos = window.pageYOffset;
+			image.style.top = ypos * .7+ 'px';
+			}
+			window.addEventListener('scroll', parallex),false;
+
+		
+		
+
+
+      
+
+  }
+
+);
+
+
+
+
