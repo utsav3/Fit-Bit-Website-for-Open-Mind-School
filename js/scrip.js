977dd3f41505f865251589ad5b8bdec930e0fbb3
utsav3 <utsav.dhungel@gmail.com>
2016-12-22 13:28:57 -0600

    Changes to nav bar

--- js/scrip.js
+++ js/scrip.js
@@ -8,40 +8,50 @@ $(document).ready(
 
   function() { 
 
-    $("html").niceScroll({
-        cursorcolor:"rgba(30,30,30,.5)",
-        zindex:999,
-        scrollspeed:100,
-        mousescrollstep:10,
-        cursorborder:"0px solid #fff",
-    });
+		$("html").niceScroll({
+			cursorcolor:"rgba(30,30,30,.5)",
+			zindex:999,
+			scrollspeed:100,
+			mousescrollstep:10,
+			cursorborder:"0px solid #fff",
+			autohidemode: false,
+		});
 	
 	
 	
-			var ypos,image;
-			function parallex() {
+		var ypos,image;
+		function parallex() {
 			image = document.getElementById('image');
 			ypos = window.pageYOffset;
-			image.style.top = ypos * .7+ 'px';
+			image.style.top = ypos * .7+ 'px';			
+		}
 			
-			
-			
-			
-			
-			}
-			window.addEventListener('scroll', parallex),false;
+		window.addEventListener('scroll', parallex),false;
 		
 		
-
-			
 		
-
-
-      
-
+			   
   }
-
+  
+ 
 );
+
+		
+
+	 // if the image in the window of browser when the page is loaded, show that image
+    $(document).ready(function(){
+           showImages('.star'); 
+    });
+
+    // if the image in the window of browser when scrolling the page, show that image
+    $(window).scroll(function() {
+            showImages('.star');
+    });
+
+
+
+
+	
 	
 
 
